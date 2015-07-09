80b15ab1f0bea74c16cc1485cb0e49d3ad5832f4
Hugo Gonzalez Labrador <hglavra@gmail.com>
2015-07-09 15:35:14 +0200

    Added EOS metadata retrieval

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -17,6 +21,7 @@ type cliFlags struct {
 	dbPort     int
 	dbName     string
 	dryRun     bool
+	eosMGMURL  string
 }
 
 func parseFlags() *cliFlags {
@@ -27,65 +32,30 @@ func parseFlags() *cliFlags {
 	flag.IntVar(&flags.dbPort, "port", 0, "The port of the db")
 	flag.StringVar(&flags.dbName, "dbname", "", "The name of the database")
 	flag.BoolVar(&flags.dryRun, "dryrun", true, "With dry run enbaled the changes are not commited to the db")
+	flag.StringVar(&flags.eosMGMURL, "eosmgmurl", "root://eospps-slave.cern.ch", "The EOS MGM URL")
 	flag.Parse()
 	return flags
 }
 
 type shareInfo struct {
-	ID          int       `db:"id"`
-	ShareType   string    `db:"share_type"`
-	ShareWith   string    `db:"share_with"`
-	UIDOwner    string    `db:"uid_owner"`
-	Parent      int       `db:"parent"`
-	ItemType    string    `db:"item_type"`
-	ItemSource  string    `db:item_source`
-	ItemTarget  string    `db:item_target`
-	FileSource  string    `db:file_source`
-	FileTarget  string    `db:file_target`
-	Permissions string    `db:permissions`
-	STime       int       `db:stime`
-	Accepted    int       `db:accepted`
-	Expiration  time.Time `db:expiration`
-	Token       string    `db:token`
-	MailSend    int       `db:mail_send`
+	ID          int            `db:"id"`
+	ShareType   int            `db:"share_type"`
+	ShareWith   sql.NullString `db:"share_with"`
+	UIDOwner    string         `db:"uid_owner"`
+	Parent      sql.NullInt64  `db:"parent"`
+	ItemType    sql.NullString `db:"item_type"`
+	ItemSource  sql.NullString `db:"item_source"`
+	ItemTarget  sql.NullString `db:"item_target"`
+	FileSource  sql.NullInt64  `db:"file_source"`
+	FileTarget  sql.NullString `db:"file_target"`
+	Permissions string         `db:"permissions"`
+	STime       int            `db:"stime"`
+	Accepted    int            `db:"accepted"`
+	Expiration  time.Time      `db:"expiration"`
+	Token       sql.NullString `db:"token"`
+	MailSend    int            `db:"mail_send"`
 }
 
-/*
-	id          | int(11)             | NO   | PRI | NULL    | auto_increment |
-| share_type  | smallint(6)         | NO   |     | 0       |                |
-| share_with  | varchar(255)        | YES  |     | NULL    |                |
-| uid_owner   | varchar(64)         | NO   |     |         |                |
-| parent      | int(11)             | YES  |     | NULL    |                |
-| item_type   | varchar(64)         | NO   | MUL |         |                |
-| item_source | varchar(255)        | YES  |     | NULL    |                |
-| item_target | varchar(255)        | YES  |     | NULL    |                |
-| file_source | bigint(20) unsigned | YES  | MUL | NULL    |                |
-| file_target | varchar(512)        | YES  |     | NULL    |                |
-| permissions | smallint(6)         | NO   |     | 0       |                |
-| stime       | bigint(20)          | NO   |     | 0       |                |
-| accepted    | smallint(6)         | NO   |     | 0       |                |
-| expiration  | datetime            | YES  |     | NULL    |                |
-| token       | varchar(32)         | YES  | MUL | NULL    |                |
-| mail_send
-	entry := &shareInfo{}
-	err := l.db.QueryRowx("SELECT * FROM links WHERE token = ?", token).StructScan(entry)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, &storage.NotExistError{err.Error()}
-		}
-		return nil, err
-	}
-
-	linfo := &link.LinkInfo{}
-	linfo.AuthID = entry.AuthID
-	linfo.Username = entry.Username
-	linfo.URI = entry.URI
-	linfo.ResourceID = entry.ResourceID
-	linfo.Resolution = entry.Resolution
-	linfo.Extra = nil
-
-	return linfo, nil
-*/
 type sqlDriver struct {
 	db *sqlx.DB
 }
@@ -100,14 +70,45 @@ func newSQLDriver(flags *cliFlags) (*sqlDriver, error) {
 }
 func (d *sqlDriver) getAllShares() ([]shareInfo, error) {
 	var entries []shareInfo
-	err := d.db.Select(&entries, "SELECT * FROM oc_share")
+	err := d.db.Select(&entries, "SELECT id,share_type,share_with,uid_owner,parent,item_type,item_source,item_target,file_source,file_target,permissions,stime,accepted,token FROM oc_share")
 	if err != nil {
 		return nil, err
 	}
 	return entries, nil
 }
+
+type Metadata struct {
+	Inode int64
+	Path  string
+}
+
+func (d *sqlDriver) getMetadataFromEOS(ID int64) (*Metadata, error) {
+	cmd := exec.Command("/usr/bin/eos", "-r", "0", "0", "file", "info", "inode:13", "-m")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(output)
+		return nil, err
+	}
+	kv := make(map[string]string)
+	raw := string(output)
+	partsBySpace := strings.Split(raw, " ") // we have [keylength.file=14 file=/eos/pps/proc/ container=3 ...}
+	for _, p := range partsBySpace {
+		partsByEqual := strings.Split(p, "=") // we have kv pairs like [ keylength.file 14]
+		if len(partsByEqual) == 2 {
+			kv[partsByEqual[0]] = partsByEqual[1]
+		}
+	}
+	inodeInt64, err := strconv.ParseInt(kv["ino"], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	m := &Metadata{Inode: inodeInt64, Path: kv["file"]}
+	return m, nil
+}
 func main() {
 	flags := parseFlags()
+	os.Setenv("EOS_MGM_URL", flags.eosMGMURL)
+	fmt.Println(flags)
 	d, err := newSQLDriver(flags)
 	if err != nil {
 		fmt.Println(err)
@@ -119,7 +120,11 @@ func main() {
 		os.Exit(1)
 	}
 	for _, s := range shares {
-		fmt.Printf("id: %s item_source:%s item_target: %s", s.ID, s.ItemSource, s.ItemTarget)
+		fmt.Printf("ID:%d SHARE_TYPE:%d ITEM_SOURCE:%s ITEM_TARGET:%s FILE_SOURCE:%d FILE_TARGET:%s\n", s.ID, s.ShareType, s.ItemSource.String, s.ItemTarget.String, s.FileSource.Int64, s.FileTarget.String)
+		out, err := d.getMetadataFromEOS(s.FileSource.Int64)
+		if err != nil {
+			fmt.Println(out, err)
+		}
 	}
 
 }
